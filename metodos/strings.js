c352a37516547380b51bdaa98ff7ac649b8eb5df
Matheus Pasti <117304327+MPasti@users.noreply.github.com>
2023-10-31 20:27:03 -0300

    Create strings.js

---
+++ metodos/strings.js
@@ -0,0 +1,19 @@
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+let text = 'We are the so-called "Vikings" from the north.';
+let text5 = "It's alright.";
+let text6 = 'The character \\ is called backslash.';
+
+let y = new String('John'); //assim é um objeto
+
+let str = 'Apple, Banana, Kiwi';
+let part = str.substring(7, 13);
+
+let text1 = 'Hello';
+let text2 = 'World';
+let text3 = text1.concat(' ', text2);
+
+let novoText = 'Please visit Microsoft and Microsoft!';
+let newText = text.replace(/Microsoft/g, 'W3Schools');
